6aeeeef01768b13c0d8dbd1766e3dc3f90972451
Travis CI <manuel.leduc@inria.fr>
2017-02-20 18:40:24 +0100

    Travis IC build scripts

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+DEPLOY_REPO="https://${GH_TOKEN}@github.com/gemoc/website.git"
+
+function main {
+	clean
+	get_current_site
+	build_site
+  deploy
+}
+
+function clean {
+	echo "cleaning _site folder"
+	if [ -d "_site" ]; then rm -Rf _site; fi
+}
+
+function get_current_site {
+	echo "getting latest site"
+	git clone --depth 1 $DEPLOY_REPO _site
+}
+
+function build_site {
+	echo "building site"
+	bundle exec jekyll build
+}
+
+function deploy {
+	echo "deploying changes"
+
+     if [ -z "$TRAVIS_PULL_REQUEST" ]; then
+         echo "except don't publish site for pull requests"
+         exit 0
+     fi
+
+     if [ "$TRAVIS_BRANCH" != "gh-page-edit" ]; then
+         echo "except we should only publish the gh-page-edit branch. stopping here"
+         exit 0
+     fi
+
+
+	cd _site
+	git config --global user.name "Travis CI"
+    git config --global user.email manuel.leduc@inria.fr
+	git add -A
+	git status
+	git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+	git push -f $DEPLOY_REPO gh-page-edit:gh-pages
+}
+
+
+main
