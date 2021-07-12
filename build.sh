71342e1cefced6776c315e3317696d85cd66fb68
ebousse <ebousse@users.noreply.github.com>
2021-07-12 13:46:49 +0000

    Deploying to master from @ gemoc/gemoc.github.io@6c9c3b84e6195345aea5c43d1f7b0607f56e8610 🚀

--- build.sh
+++ build.sh
@@ -3,13 +3,9 @@
 set -e
 set -x
 
-DEPLOY_REPO="https://${GH_TOKEN}@github.com/gemoc/gemoc.github.io.git"
-
 function main {
 	clean
-	get_current_site
 	build_site
-    deploy
 }
 
 function clean {
@@ -17,40 +13,9 @@ function clean {
 	if [ -d "_site" ]; then rm -Rf _site; fi
 }
 
-function get_current_site {
-	echo "getting latest site"
-	git clone --depth 1 $DEPLOY_REPO _site
-}
-
-
 function build_site {
 	echo "building site"
 	bundle exec jekyll build
 }
-
-function deploy {
-	echo "deploying changes"
-
-     if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-     then
-         echo "Pull request are not deployed"
-         exit 0
-     fi
-
-     if [ "$TRAVIS_BRANCH" != "gh-pages-edit" ]; then
-         echo "except we should only publish the gh-page-edit branch. stopping here"
-         exit 0
-     fi
-
-
-	cd _site
-	git config --global user.name "Travis CI"
-    git config --global user.email didier.vojtisek@inria.fr
-	git add -A
-	git status
-	git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
-	git push -f $DEPLOY_REPO HEAD:master
-}
-
 
 main
